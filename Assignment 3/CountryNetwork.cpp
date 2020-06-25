e1333d2a660ad1fb2ac730a35423a87f6347bedf
sesa3153 <47164989+sesa3153@users.noreply.github.com>
2020-06-25 10:30:46 -0600

    Added the first two functions
    I added the first function that initializes the head node as a NULL. I also added the second function which creates a new node and inserts into the function. This algorithm is not correct however as it appends nodes instead of appending (Needs to be fixed)

--- Assignment 3/CountryNetwork.cpp
+++ Assignment 3/CountryNetwork.cpp
@@ -1,1 +1,39 @@
+#include "CountryNetwork.hpp"
+#include <iostream>
+#include <list>
+using namespace std;
+
+CountryNetwork::CountryNetwork()
+{
+
+  head = NULL;
+
+};
+
+void CountryNetwork::insertCountry(Country* previous, string countryName)
+{
+
+  Country* node = new Country;
+  Country* current;
+  node->name = countryName;
+  node->numberMessages = 0;
+
+  if (previous != NULL)
+  {
+    current = previous;
+    while(current -> next !=NULL)
+    {
+      current = current->next;
+    }
+    current->next = node;
+    cout << "adding: " << countryName << " (prev: " << current->name << ")" << endl;
+  }
+  else
+  {
+    head = node;
+    cout << "adding: " << countryName << " (Head)" << endl;
+  }
+
+};
+
 
