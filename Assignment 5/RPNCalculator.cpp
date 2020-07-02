bc16764ca19ebdd80a98439a095c8f232053e7f1
sesa3153 <47164989+sesa3153@users.noreply.github.com>
2020-07-02 10:19:45 -0600

    added the third and fourth member functions of the class
    The fourth member function checks to determine whether the stack data structure is empty. The fourth member function adds a node to the top of the stack.

--- Assignment 5/RPNCalculator.cpp
+++ Assignment 5/RPNCalculator.cpp
@@ -26,3 +26,45 @@ RPNCalculator::~RPNCalculator() {
 
     stackHead = NULL;
 };
+
+
+/* INPUT: No input
+   Modify: No modification
+   Output: Boolean value (determines whether stack is empty) */
+bool RPNCalculator::isEmpty() {
+
+  if (stackHead == NULL) {
+
+    return 1;
+
+  }
+
+  else {
+
+    return 0;
+
+  }
+};
+
+/*INPUT: A float datatype
+  Modify: Inserts a node at the top of the stack with the input data
+  OUTPUT: No output */
+void RPNCalculator::push(float number) {
+
+  Operand* Node = new Operand;
+
+  Node->number = number;
+
+  if (stackHead != NULL) {
+
+    Node->next = stackHead;
+
+    stackHead = Node;
+
+  } else {
+
+    stackHead = Node;
+
+  }
+
+};
