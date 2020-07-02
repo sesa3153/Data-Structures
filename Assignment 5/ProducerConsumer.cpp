70e25b8380959c141a902f4d06326a96635a9ab3
sesa3153 <47164989+sesa3153@users.noreply.github.com>
2020-07-02 10:49:10 -0600

    Added the 2nd and 3rd member functions
    The 2nd member function determines whether the queue is full or not. The 3rd member function adds items to the queue if the queue is not full.

--- Assignment 5/ProducerConsumer.cpp
+++ Assignment 5/ProducerConsumer.cpp
@@ -30,3 +30,39 @@ bool ProducerConsumer::isEmpty() {
   }
 
 };
+
+
+/* INPUT: No input
+   Modify: No Modification
+   OUTPUT: returns a boolean value that determines whether the array is Full */
+bool ProducerConsumer::isFull() {
+
+  if ((queueEnd+1)%SIZE == queueFront) {
+
+    return 1;
+
+  } else {
+
+    return 0;
+
+  }
+
+};
+
+/* INPUT: Takes in a string data type
+   Modify: Appends the string to the rear of the queue if it is not full
+   OUTPUT: No output */
+void ProducerConsumer::enqueue(string item) {
+
+  if(isFull()) {
+
+    cout << "The Queue is full, cannot add new item" << endl;
+
+  } else {
+
+    queue[queueEnd] = item;
+
+    queueEnd = (queueEnd+1)%SIZE;
+  }
+
+};
